bc21ca86fbf046a79ba8dd75e05c64ec19ce2aea
kattbaum <katt.baum@gmail.com>
2019-09-03 17:03:51 -0400

    Fixed character issue

--- bootcamp-prep-master/pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+function lastCharacter(str1, str2) {
+    if (str1.charAt(str1.length - 1) === str2.charAt(str2.length - 1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
