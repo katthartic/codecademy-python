ad0605b2843ec0e01e465175f4242cc5e1aa1ea2
kattbaum <katt.baum@gmail.com>
2019-09-03 16:08:22 -0400

    Solved 02-do-you-play-the-theremin problem

--- bootcamp-prep-master/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(name) {
+    if (name.charAt(0) === 's' || name.charAt(0) === 'S') {
+        return true;
+    } else {
+        return false;
+    }
+}
