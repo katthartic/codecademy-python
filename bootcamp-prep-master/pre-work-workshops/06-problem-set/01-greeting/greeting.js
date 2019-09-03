175c0edb4da1e14523c64c718ae871c2ffa4fc6c
kattbaum <katt.baum@gmail.com>
2019-09-03 15:24:35 -0400

    Solved 01-greeting problem

--- bootcamp-prep-master/pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function greeting(name) {
+    if (name) {
+        return `Hello ${name}!`;
+    } else {
+        return 'Hello!';
+    }
+}
+
+/*
+function greeting(name) {
+    greeting = ''
+    name ? greeting = `Hello ${name}!` : greeting = 'Hello!';
+    return greeting
+}
+*/
