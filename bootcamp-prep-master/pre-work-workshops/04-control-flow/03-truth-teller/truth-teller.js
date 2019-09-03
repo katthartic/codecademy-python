d9c14d46d6e9f260be20e587f84dc14d9108c090
kattbaum <katt.baum@gmail.com>
2019-09-03 12:24:48 -0400

    Solved truth-teller in threes ways

--- bootcamp-prep-master/pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ bootcamp-prep-master/pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -6,10 +6,27 @@ let boolean1 = true, boolean2 = true;
 let result;
 
 // YOUR CODE BELOW
+/*
 if (boolean1 === true && boolean2 === true) {
     result = 'both';
-} else if ((boolean1 === false || boolean2 === false) && (boolean1 === true || boolean2 === true)) {
+} else if (boolean1 === true || boolean2 === true) {
     result = 'one';
 } else {
     result = 'none';
 }
+*/
+
+/*
+if (boolean1 && boolean2) {
+    result = 'both';
+} else if (boolean1 || boolean2) {
+    result = 'one';
+} else {
+    result = 'none';
+}
+*/
+
+
+(boolean1 && boolean2) ? result = 'both' : //returns 'both' if both variables are true
+(boolean1 || boolean2) ? result = 'one' : //returns 'one' if only one variable is true
+result = 'none'; //returns 'none' if both variables are false
