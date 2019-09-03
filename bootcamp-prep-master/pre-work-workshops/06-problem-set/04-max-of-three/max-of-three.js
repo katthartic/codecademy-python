9d3271873d55c97885c7c59a93af14e6f53cce0c
kattbaum <katt.baum@gmail.com>
2019-09-03 17:04:18 -0400

    Completed exercises

--- bootcamp-prep-master/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function maxOfThree(num1,num2,num3) {
+    let finalNum = num1
+    if (finalNum < num2) {
+        finalNum = num2;
+    } 
+    if (finalNum < num3) {
+        finalNum = num3;
+    }
+    return finalNum;
+}
--- bootcamp-prep-master/pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ bootcamp-prep-master/pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function everyWhichWay(num1, num2, num3) {
+    switch (num3) {
+        case (num1 + num2):
+            return 'sum';
+            break;
+        case (num1 - num2):
+            return 'difference';
+            break;
+        case (num1 * num2):
+            return 'product';
+            break;
+        case (num1 / num2):
+            return 'fraction';
+            break;
+        default:
+            return null;
+            break;
+    }
+}
