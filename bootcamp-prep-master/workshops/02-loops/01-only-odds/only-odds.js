c2efa854b320cb20342b091c45aaa447cca3fd01
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-07 13:54:59 -0400

    Install test and solved 02-loops Ex 01 & 02

--- bootcamp-prep-master/workshops/02-loops/01-only-odds/only-odds.js
+++ bootcamp-prep-master/workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function onlyOdds (num) {
+    let sum = 0;
+    for (i = 0; i < num; i++) {
+        if (i % 2 === 1) {
+            sum += i;
+        }
+    }
+    return sum;
+
+}
--- bootcamp-prep-master/workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ bootcamp-prep-master/workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+// function crazyCaps (beginStr) {
+//     let finalStr = '';
+//     let i = 0;
+//     while (i < beginStr.length) {
+//         if (i % 2 === 0 ) {
+//             finalStr += beginStr[i].toLowerCase();
+//         } else {
+//             finalStr += beginStr[i].toUpperCase();
+//         }
+//         i++;
+//     }
+//     return finalStr;
+// }
+
+function crazyCaps (beginStr) {
+    let finalStr = ''
+    for (let i = 0 ; i < beginStr.length ; i++) {
+        if (i % 2 === 0 ) {
+            finalStr += beginStr[i].toLowerCase();
+        } else {
+            finalStr += beginStr[i].toUpperCase();
+        }
+    }
+    return finalStr;
+}
