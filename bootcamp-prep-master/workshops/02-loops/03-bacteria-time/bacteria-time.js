0399478b525287dfe48e819b2f92ae06def8404f
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-07 15:12:53 -0400

    Solved 02-loops Exercises 03 and 04

--- bootcamp-prep-master/workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ bootcamp-prep-master/workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function bacteriaTime (currentNum, targetNum) {
+    let finalTime = 0
+    if (targetNum < currentNum) {
+      return 'targetNum must be larger than currentNum'
+    }
+    while (currentNum < targetNum) {
+      currentNum *= 2
+      finalTime += 20
+    }
+    return finalTime
+  }
--- bootcamp-prep-master/workshops/02-loops/04-exponentiate/exponentiate.js
+++ bootcamp-prep-master/workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function exponentiate (base, power) {
+    let result = base
+    if (power === 0 ) {
+      return 1
+    }
+    else {
+      for (let i=1 ; i<power ; i++) {
+        result *= base
+      }
+    }
+    return result
+  }
