cd1682d3f0fdb5e76f54078c249497b31cf38fdb
Katt Baum <mkb2@KBs-MacBook-Pro.home>
2019-09-07 22:20:06 -0400

    Solved 03 exercises

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function howEqual (type1, type2) {
+    if (type1 === type2) {
+      return 'strictly'
+    }
+    else if (type1 == type2) {
+      return 'loosely'
+    }
+    return 'not equal'
+  }
--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function isTruthy (arg) {
+    if(!arg === false) {
+      return true
+    } 
+    if (typeof arg === 'boolean') {
+      return 'The boolean value false is falsey'
+    }
+    if (typeof arg === 'object') {
+      return 'The null value is falsey'
+    }
+    if (typeof arg === 'undefined') {
+      return 'undefined is falsey'
+    }
+    if (typeof arg === 'number') {
+      return 'The number 0 is falsey (the only falsey number)'
+    }
+    if (typeof arg === 'string') {
+      return 'The empty string is falsey (the only falsey string)'
+    }
+  }
