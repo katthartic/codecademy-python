8475dda018216c1e4be6d3a188aaf1da68bc3f47
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-08 17:12:47 -0400

    Installed test and solved 01,02,03

--- bootcamp-prep-master/workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ bootcamp-prep-master/workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function oddCouple(array1) {
+    let newArray = []
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] % 2 === 1  && newArray.length < 2) {
+            newArray.push(array1[i]);
+        }
+    }
+    return newArray;
+}
--- bootcamp-prep-master/workshops/05-arrays-i/02-my-includes/my-includes.js
+++ bootcamp-prep-master/workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+    for (let i = 0; i < array.length; i++ ){
+        if (array[i] === searchValue ) {
+            return true
+        }
+    }
+    return false
+}
