7bf6c521e92abe83aaca5f2dabf29c7a79e04d82
Katt Baum <mkb2@Bauers-iPhone.home>
2019-09-08 19:44:47 -0400

    Solved exercise

--- bootcamp-prep-master/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function arrayFlattener (array) {
+    finalArray = []
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        finalArray.push(...array[i])
+      } else {
+        finalArray.push(array[i])
+      }
+    }
+    return finalArray
+}
