33490a0d3eee68e6e2f576e8891ac579df6720c8
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-09 18:54:59 -0400

    Typechange and re-install of test

--- bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function zooInventory(array) {
+    // for (let i1 = 0; i1 < array.length; i1++) {
+    //     let description = ''
+    //     let name = ''
+    //     let animal = ''
+    //     let age = ''
+    //     if (Array.isArray.array[i1] === false) {
+    //         name = array[i1]
+    //     } else {
+    //         for (let i2 = 0; i2 < array[i2].length; i2++) {
+                
+    //         }
+    //     }
+    // }
+}
