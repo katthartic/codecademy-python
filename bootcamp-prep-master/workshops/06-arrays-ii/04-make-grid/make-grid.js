641df0caf8f75f3f0c3304b7851206c9f73fbbf6
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-09 21:32:56 -0400

    Solved exercise

--- bootcamp-prep-master/workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function makeGrid (numColumns, numRows) {
+    let arr = []
+    let grid = []
+    for (i = 1; i <= numColumns; i++) {
+      arr.push(i)
+    }
+    for (j = 0; j < numRows; j++){
+      grid.push(arr)
+    }
+    return grid
+  }
