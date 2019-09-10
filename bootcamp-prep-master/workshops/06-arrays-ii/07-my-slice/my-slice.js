68cb56bb2930922a8a55b3602fdcd13b9bc435e7
kattbaum <katt.baum@gmail.com>
2019-09-10 17:02:06 -0400

    Refactored exercise

--- bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,26 +1,42 @@
 // YOUR CODE BELOW
-const mySlice = (originalArray, startIdx = 0, endIdx = originalArray.length) => {
-    newArray = []
-    if (startIdx < 0 && endIdx < 0) {
-      for (i = originalArray.length + startIdx; i < originalArray.length + endIdx; i++){
-      newArray.push(originalArray[i])
-      }
-      return newArray
-    }
-    if (startIdx < 0) {
-      for (i = originalArray.length + startIdx; i < endIdx; i++){
-      newArray.push(originalArray[i])
-      }
-      return newArray
-    }
-    if (endIdx < 0) {
-      for (i = startIdx; i < originalArray.length + endIdx; i++){
-      newArray.push(originalArray[i])
-      }
-      return newArray
-    }
-    for (i = startIdx; i < endIdx; i++){
-      newArray.push(originalArray[i])
-    }
-    return newArray
+//CONDENSED VERSION!!! SO GOOD!!!
+function mySlice (originalArray, startIdx = 0, endIdx = originalArray.length) {
+  newArray = []
+  if (startIdx < 0) {
+    startIdx += originalArray.length
   }
+  if (endIdx < 0) {
+    endIdx += originalArray.length
+  }
+  for (i = startIdx; i < endIdx; i++){
+    newArray.push(originalArray[i])
+  }
+  return newArray
+}
+
+//FIRST ATTEMPT
+// const mySlice = (originalArray, startIdx = 0, endIdx = originalArray.length) => {
+//     newArray = []
+//     if (startIdx < 0 && endIdx < 0) {
+//       for (i = originalArray.length + startIdx; i < originalArray.length + endIdx; i++){
+//       newArray.push(originalArray[i])
+//       }
+//       return newArray
+//     }
+//     if (startIdx < 0) {
+//       for (i = originalArray.length + startIdx; i < endIdx; i++){
+//       newArray.push(originalArray[i])
+//       }
+//       return newArray
+//     }
+//     if (endIdx < 0) {
+//       for (i = startIdx; i < originalArray.length + endIdx; i++){
+//       newArray.push(originalArray[i])
+//       }
+//       return newArray
+//     }
+//     for (i = startIdx; i < endIdx; i++){
+//       newArray.push(originalArray[i])
+//     }
+//     return newArray
+//   }
