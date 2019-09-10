041453a300410b5dc3709382f5ff882bcd4248e4
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-09 20:23:27 -0400

    Solved 06 exercise

--- bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,16 +1,12 @@
 // YOUR CODE BELOW
-function zooInventory(array) {
-    // for (let i1 = 0; i1 < array.length; i1++) {
-    //     let description = ''
-    //     let name = ''
-    //     let animal = ''
-    //     let age = ''
-    //     if (Array.isArray.array[i1] === false) {
-    //         name = array[i1]
-    //     } else {
-    //         for (let i2 = 0; i2 < array[i2].length; i2++) {
-                
-    //         }
-    //     }
-    // }
-}
+function zooInventory(arr) {
+    let flatArr = []
+    for (let i = 0; i < arr.length; i++) {
+      const name = arr[i][0]
+      const animal = arr[i][1][0]
+      const age = arr[i][1][1]
+      const description = `${name} the ${animal} is ${age}.`
+      flatArr.push(description)
+    }
+    return flatArr
+  }
