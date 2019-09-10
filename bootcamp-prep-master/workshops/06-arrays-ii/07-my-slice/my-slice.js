b5372f54b90eac1fc9e96ccb4d51a83bbe0d356a
kattbaum <katt.baum@gmail.com>
2019-09-10 16:39:48 -0400

    solved exercise

--- bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+const mySlice = (originalArray, startIdx = 0, endIdx = originalArray.length) => {
+    newArray = []
+    if (startIdx < 0 && endIdx < 0) {
+      for (i = originalArray.length + startIdx; i < originalArray.length + endIdx; i++){
+      newArray.push(originalArray[i])
+      }
+      return newArray
+    }
+    if (startIdx < 0) {
+      for (i = originalArray.length + startIdx; i < endIdx; i++){
+      newArray.push(originalArray[i])
+      }
+      return newArray
+    }
+    if (endIdx < 0) {
+      for (i = startIdx; i < originalArray.length + endIdx; i++){
+      newArray.push(originalArray[i])
+      }
+      return newArray
+    }
+    for (i = startIdx; i < endIdx; i++){
+      newArray.push(originalArray[i])
+    }
+    return newArray
+  }
