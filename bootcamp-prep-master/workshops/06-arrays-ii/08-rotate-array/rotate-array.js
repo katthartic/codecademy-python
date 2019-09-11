8fdbeb101f745564ec3bf2d1172824b9ffd2047d
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-10 20:15:09 -0400

    Installed npm test

--- bootcamp-prep-master/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function rotateArray (originalArray, rotateNum) {
+    let newArrPart = []
+    if (rotateNum > 0){
+      let newArrPart = originalArray.splice(-rotateNum)
+      console.log(originalArray)
+      console.log(newArrPart)
+      return newArrPart.concat(originalArray)
+    }
+      if (rotateNum < 0){
+      let newArrPart = originalArray.splice(-rotateNum)
+      console.log(originalArray)
+      console.log(newArrPart)
+      return newArrPart.concat(originalArray)
+    }
+  }
