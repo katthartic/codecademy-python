b8978c6d00d334e6f638e9b6c5edb0d035ae2847
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-11 19:36:04 -0400

    Attempted exercise

--- bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(string) {
+    let key = []
+    let val = []
+    for (let i = 0; i < string.length; i++) {
+        if (keys.includes(string[i] === false)) {
+            keys.push(string[i])
+        }
+    }
+}
