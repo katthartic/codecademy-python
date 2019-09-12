acf9303944a774472f9d9a5b11ca6a6f0627c5eb
kattbaum <katt.baum@gmail.com>
2019-09-12 15:10:46 -0400

    Solved exercise

--- bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,10 +1,29 @@
 // YOUR CODE BELOW
 function frequencyAnalysis(string) {
-    let key = []
-    let val = []
+    let obj = {}
     for (let i = 0; i < string.length; i++) {
-        if (keys.includes(string[i] === false)) {
-            keys.push(string[i])
+        let char = string[i]
+        if (char in obj === false) {
+            obj[char] = 1
+            // console.log(obj)
+        } else {
+            obj[char]++
         }
     }
+    return obj
 }
+
+
+// function frequencyAnalysis(string) {
+//     let obj = {}
+//     for (let i = 0; i < string.length; i++) {
+//         let char = string[i]
+//         let count = 0
+//             for (j = 0; j < string.length; j++) {
+//                 if (char === string[j])
+//                     count++
+//             }
+//         obj.char = count
+//     }
+//     return obj
+// }
