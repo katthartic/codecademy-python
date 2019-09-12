898304de3d21ebb0e891b379b10514c3589cadf2
kattbaum <katt.baum@gmail.com>
2019-09-12 15:34:04 -0400

    Solved exercise

--- bootcamp-prep-master/workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ bootcamp-prep-master/workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,11 +1,12 @@
 // YOUR CODE BELOW
-function callThemAll(obj,value){  let results = []  for(key in obj){
+function callThemAll(obj,value) {
+  let results = []
+  for(key in obj) {
     const val = obj[key]
-      // console.log(typeof val)
-      if (typeof val==='function'){
-       let res = val(value)
-       results.push(res)
-      }
+    if (typeof val==='function'){
+      let res = val(value)
+      results.push(res)
     }
-    return results
-   }
+  }
+  return results
+ }
