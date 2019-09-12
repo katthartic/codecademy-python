97c173dcef1f0e2d79f0782bd8ee9bb19fe0f4f5
kattbaum <katt.baum@gmail.com>
2019-09-12 15:19:16 -0400

    Solved exercise

--- bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function dogBreeder(name = 'Steve', age = 0) {
+    let dogObj = {}
+    if (typeof name === 'string') {
+        dogObj.name = name
+        dogObj.age = age
+    } else {
+        dogObj.name = 'Steve'
+        dogObj.age = name
+    }
+    return dogObj
+}
