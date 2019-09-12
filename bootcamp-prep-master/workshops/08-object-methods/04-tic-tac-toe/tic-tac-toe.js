0a6e7ced781f2b8286a31f73a3e7c3e946b61933
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-12 19:46:21 -0400

    Solved ticTacToe

--- bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -4,19 +4,17 @@ let ticTacToe = {
             [null, null, null],
             [null, null, null]],
     move: function(character, rowNum, colNum){
-      if (rowNum === 0 && colNum === 0){
-        return ticTacToe.board
-      } else if (ticTacToe.board[rowNum][colNum] === null) {
-        ticTacToe.board[rowNum][colNum] = character
-        return ticTacToe.board
+      if (!this.board[rowNum][colNum]) {
+        this.board[rowNum][colNum] = character
+        return this.board
       } else {
-        return ticTacToe.board
+        return this.board
       }
     },
     clear: function(){
-      ticTacToe.board = [[null, null, null],
+      this.board = [[null, null, null],
             [null, null, null],
             [null, null, null]]
-      return ticTacToe.board
+      return this.board
     }
    };
