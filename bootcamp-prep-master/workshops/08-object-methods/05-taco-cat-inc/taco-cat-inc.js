49a93e02cf5b85f9b26542ff557eaf15f9fd69f1
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-12 19:46:47 -0400

    started tacoCatInc

--- bootcamp-prep-master/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ bootcamp-prep-master/workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -19,7 +19,24 @@ let tacoCatInc = {
     'treat dust': {cost: 0.1, quantity: 100}
   },
 
+  currentInventory: function() {
+    let grandTotal = 0
+    for (let key in this) {
+      const category = this[key]
+      if (typeof category === 'object'){
+        for (let food in category) {
+          const data = category[food]
+          const total = data.cost * data.quantity
+          grandTotal += total
+        }
+      }
+    }
+    return grandTotal
+  },
+
+  sale: function() {},
+
   cash: 0
 };
 
-// YOUR CODE BELOW
+
