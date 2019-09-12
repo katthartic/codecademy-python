1708e7a4099a086e8a3313e249b10bf00dcc193e
kattbaum <katt.baum@gmail.com>
2019-09-12 15:34:26 -0400

    Attempted exercise

--- bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+    board: [[null, null,null],
+            [null, null,null],
+            [null, null,null]],
+    move: function(character, rowNum, colNum){
+        ticTacToe.board[rowNum-1][colNum-1] = character
+        return ticTacToe.board
+    },
+    clear: function(){}
+   };
