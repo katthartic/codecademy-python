35dd2fcc9a2749da55917cf316745612220980db
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-12 20:25:45 -0400

    Solved 01-very-odd/very-odd.js

--- bootcamp-prep-master/workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ bootcamp-prep-master/workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function veryOdd (arr) {
+    newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i]
+        if (num % 2 === 1) {
+            newArr.push(num)
+        }
+    }
+    return newArr
+}
