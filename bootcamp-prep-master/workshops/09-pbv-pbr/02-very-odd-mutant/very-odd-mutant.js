885619a803aab6836be4c21aec2bb5f3ae24f497
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-12 20:45:30 -0400

    Solved very-odd-mutant

--- bootcamp-prep-master/workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ bootcamp-prep-master/workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function veryOddMutant(arr) {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i]
+        if (num % 2 === 0) {
+            arr[i] = 'normie'
+            count ++
+        }
+    }
+    return count
+}
