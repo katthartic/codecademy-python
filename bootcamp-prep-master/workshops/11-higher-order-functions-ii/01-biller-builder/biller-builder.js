84ebe35de82600527eb4a6ab355b1242bf46e9ae
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-14 15:59:54 -0400

    Type change

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+const billerBuilder = (state) => {
+    let shipping = 0
+    let salesTax = 0
+
+    if (state = 'NY') {
+        shipping = .03
+        salesTax = .05
+    }
+    if (state = 'NJ') {
+        shipping = .05
+        salesTax = .06625
+
+    return (price) => {
+        
+    }
+}
