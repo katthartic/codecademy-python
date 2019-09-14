e50f8c15697ca9a90e636c12161d321228c4377c
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-14 10:33:06 -0400

    Solved clone-machince exercise

--- bootcamp-prep-master/workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ bootcamp-prep-master/workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+//Class solution
+function cloneMachine(parent) {
+    const clone = {...parent }
+    clone.name = parent.name + 'Clone'
+    parent.offspring.push(clone.name)
+    clone.offspring = [] // method 1
+    //clone.offspring = [...parent.offspring] // method 2... doesn't seem to work
+    return clone
+  }
+  
+  // function cloneMachine(objA){
+  //   let objB = {...obj}
+     
+  //   objB.name = objB.name +'Clone'
+  //   // obj.offspring = []
+  //   obj.offspring.push(objB.name)
+  //   objB.offspring = []
+  //   return objB
+  // }
