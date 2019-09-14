34259733199383fd151565bc573422274f6d022d
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-14 14:34:50 -0400

    Solved 03-whos-a-special/whos-a-special.js

--- bootcamp-prep-master/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ bootcamp-prep-master/workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+//with out .forEach
+// function whosASpecial (animals) {
+//   for (let i = 0; i < animals.length; i++) {
+//     animal = animals[i]
+//     console.log(`${animal.name} the ${animal.species} is very special!`)
+//   }
+// }
+
+// with .forEach - Method 1
+// function whosASpecial (pets) {
+//   let str = ''
+//   pets.forEach((pet) => {
+//     const compliment = `${pet.name} the ${pet.species} is very special! `
+//     str += compliment
+//   })
+//   return str.slice(0,-1)
+// }
+
+//with .forEach - Method 2
+function whosASpecial (pets) {
+    let praises = []
+    pets.forEach((pet) => {
+      const praise = `${pet.name} the ${pet.species} is very special!`
+      praises.push(praise)
+    })
+    return praises.join(' ')
+  }
