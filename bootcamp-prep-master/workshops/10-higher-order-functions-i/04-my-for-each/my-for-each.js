95623c0f696934d14d8c50d95f3ee2d3dc3f89cd
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-15 11:15:02 -0400

    solved 10-higher-order-functions-i/04-my-for-each/my-for-each.js

--- bootcamp-prep-master/workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
+++ bootcamp-prep-master/workshops/10-higher-order-functions-i/04-my-for-each/my-for-each.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+const myForEach = (arr, callback) => {
+  
+    for (let i = 0; i < arr.length; i++) {
+        //callback(arr[i], i)
+        const element = arr[i]
+        callback(element, i)
+    }
+  }
