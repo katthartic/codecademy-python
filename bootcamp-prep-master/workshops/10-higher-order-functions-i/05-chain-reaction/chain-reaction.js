e9186e21dc1108b99fa9745ca80d14d7ecffd4c8
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-15 12:10:09 -0400

    Solved 10-higher-order-functions-i/05-chain-reaction/chain-reaction.js

--- bootcamp-prep-master/workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
+++ bootcamp-prep-master/workshops/10-higher-order-functions-i/05-chain-reaction/chain-reaction.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function chainReaction(startVal, arr) {
+    let finalVal = startVal
+    for (let i = 0; i < arr.length; i++) {
+        fn = arr[i]
+        result = fn(finalVal)
+        finalVal = result
+    }
+    return finalVal
+}
