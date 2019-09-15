cd98fe05bd93f2722c6bfb252f16bd1cabb9509c
Katt Baum <mkb2@KBs-MacBook-Pro.local>
2019-09-15 10:51:38 -0400

    Added class solution to 11-higher-order-functions-ii/07-cache-savings/cache-savings.js

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,12 +1,23 @@
-// YOUR CODE BELOW
+// My Solution
+// const cacheSavings = (callback) => {
+//     results = []
+//       return (arg) => {
+//         for (let i = 0; i < results.length; i++) {
+//           if (results[i].arg === arg) return results[i].result
+//         }
+//         result = callback(arg)
+//         results.push({arg: arg, result: result})
+//         return result
+//       }
+//   }
+
+//Class Solution
 const cacheSavings = (callback) => {
-    results = []
-      return (arg) => {
-        for (let i = 0; i < results.length; i++) {
-          if (results[i].arg === arg) return results[i].result
-        }
-        result = callback(arg)
-        results.push({arg: arg, result: result})
+    cache = {}
+    return (arg) => {
+        if (arg in cache) return cache[arg]
+        const result = callback(arg)
+        cache[arg] = result
         return result
-      }
-  }
+    }
+}
