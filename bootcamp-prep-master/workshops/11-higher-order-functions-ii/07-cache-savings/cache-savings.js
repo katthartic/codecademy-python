3f31640ebe1333bea0d1253f436414c27e2cd5ee
Katt Baum <mkb2@Dink.home>
2019-09-15 08:32:24 -0400

    Solved 11-higher-order-functions-ii/07-cache-savings/cache-savings.js

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+const cacheSavings = (callback) => {
+    results = []
+      return (arg) => {
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].arg === arg) return results[i].result
+        }
+        result = callback(arg)
+        results.push({arg: arg, result: result})
+        return result
+      }
+  }
