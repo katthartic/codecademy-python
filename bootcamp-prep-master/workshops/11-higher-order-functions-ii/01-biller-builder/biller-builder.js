e534ec9c34679ed9a83076756dee114aea5ee1c6
Katt Baum <mkb2@Dink.home>
2019-09-15 07:48:27 -0400

    Solved 11-higher-order-functions-ii/01-biller-builder/biller-builder.js

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -3,15 +3,18 @@ const billerBuilder = (state) => {
     let shipping = 0
     let salesTax = 0
 
-    if (state = 'NY') {
-        shipping = .03
-        salesTax = .05
+    if (state === 'NY') {
+        shipping = 1.03
+        salesTax = 1.04
     }
-    if (state = 'NJ') {
-        shipping = .05
-        salesTax = .06625
+
+    if (state === 'NJ') {
+        shipping = 1.05
+        salesTax = 1.06625
+    }
 
     return (price) => {
-        
+        let total = price * shipping * salesTax
+        return total
     }
 }
