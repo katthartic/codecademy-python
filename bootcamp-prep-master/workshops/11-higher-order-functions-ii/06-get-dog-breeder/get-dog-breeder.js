d3d6c37dc7238aad26fb119bdf338d92897436dd
Katt Baum <mkb2@Dink.home>
2019-09-15 08:12:49 -0400

    Solved 11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+const getDogBreeder = (defaultName, defaultAge) => {
+    return function dogBreeder (name = defaultName, age = defaultAge) {
+        let dogObj = {}
+        if (typeof name === 'string') {
+            dogObj.name = name
+            dogObj.age = age
+        } else {
+            dogObj.name = defaultName
+            dogObj.age = name
+        }
+        return dogObj
+    }
+}
